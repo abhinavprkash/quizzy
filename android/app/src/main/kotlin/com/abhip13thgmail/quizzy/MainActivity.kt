4fab09b7ef1c2eab2f722385466f3dea13dcbe2b
abhinavprkash <51023991+abhinavprkash@users.noreply.github.com>
2019-09-12 15:34:45 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/abhip13thgmail/quizzy/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.abhip13thgmail.quizzy
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
